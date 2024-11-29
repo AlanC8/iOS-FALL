0ba288e1d92755ef9ab7652495f540893208f34a
AlanC8 <askvizi@bk.ru>
2024-11-29 22:01:34 +0500

    11

---
+++ task11/task11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task11
+//
+//  Created by Алан Абзалханулы on 29.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
